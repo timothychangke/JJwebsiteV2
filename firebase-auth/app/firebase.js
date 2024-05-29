62a28f54430827ef33c2d9427945feb01afacca5
a'qil <131641361+qilstiano@users.noreply.github.com>
2024-05-29 20:55:52 +0800

    integrated Firebase Authentication via Google Sign-In
    Firebase Authentication via Google Sign-In added in firebase.js
    
    minor changes to basic UI to reflect sign-in status
    
    added API keys to .env (oops)
    
    removed node_modules

--- firebase-auth/app/firebase.js
+++ firebase-auth/app/firebase.js
@@ -1,19 +1,24 @@
+import dotenv from 'dotenv';
+dotenv.config();
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 
+console.log(process.env.NEXT_PUBLIC_API_KEY)
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAhMTTg2KTfm4RtTOUdKkrN7U7j9rZNkJk",
-  authDomain: "auth-jj.firebaseapp.com",
-  projectId: "auth-jj",
-  storageBucket: "auth-jj.appspot.com",
-  messagingSenderId: "61044582802",
-  appId: "1:61044582802:web:c5d55829f5fa0e8612b94b"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJ_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
+console.log(process.env);
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
