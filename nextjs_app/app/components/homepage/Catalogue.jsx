d411690a9ee884f55ff105b0146fa0478cb3334e
Jia Wen <90663992+jiawendh@users.noreply.github.com>
2024-07-15 21:23:43 +0800

    feat/homepage

--- nextjs_app/app/components/homepage/Catalogue.jsx
+++ nextjs_app/app/components/homepage/Catalogue.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { CarouselNext, CarouselPrevious, CarouselItem } from "@/components/ui/carousel";
-import { ResponsiveImage } from "./ResponsiveImage";
+import { TabsTrigger } from "@/components/ui/tabs"
+import { ResponsiveImage } from "../ResponsiveImage";
 
 export const CatalogueTrigger = (props) => {
     return (
-        <TabsTrigger value={props.value} className="block text-white font-light data-[state=active]:text-white data-[state=active]:bg-transparent data-[state=active]:shadow-none data-[state=active]:font-bold">
+        <TabsTrigger key={props.index} value={props.value} className="block text-white font-light data-[state=active]:text-white data-[state=active]:bg-transparent data-[state=active]:shadow-none data-[state=active]:font-bold">
             <p className='text-lg'>{props.title}</p>
-            <div className='h-px bg-white mx-auto'></div>
+            <div className='h-0.5 w-full md:w-44 bg-white mx-auto'></div>
         </TabsTrigger>
     )
 }
@@ -21,18 +22,18 @@ export const CatalogueNext = () => {
 
 export const CatalogueItem = (props) => {
     return (
-        <CarouselItem className="md:basis-1/2 lg:basis-1/3 mb-5" key={props.key}>
-            <div className="relative bg-beige border-dark-green border-4 rounded-lg px-4 pt-4 pb-28 h-full flex flex-col items-center">
+        <CarouselItem className="sm:basis-1/2 md:basis-1/3 mb-5" key={props.index}>
+            <div className="relative bg-beige border-dark-green border-[6px] rounded-lg px-4 pt-4 pb-28 h-full flex flex-col items-center">
                 {props.image ? (
-                    <ResponsiveImage mobileSrc={props.image} desktopSrc={props.image} mobileSize={100} desktopSize={150}></ResponsiveImage>
+                    <ResponsiveImage mobileSrc={props.image} desktopSrc={props.image} mobileSize={80} desktopSize={80}></ResponsiveImage>
                 ) : (<></>)}
                 <div className='text-center leading-snug flex flex-col justify-between h-full'>
                     <div className='grow justify-center items-center flex'>
-                        <h3 className='text-3xl font-bold'>{props.title}</h3>
+                        <h3 className='text-xl font-bold'>{props.title}</h3>
                     </div>
-                    <p className='text-black mt-3'>{props.description}</p>
+                    <p className='text-black mt-3 text-sm'>{props.description}</p>
                     <div className='absolute left-0 -bottom-5 right-0'>
-                        <div className='text-black mb-3 font-bold h-16 flex flex-col justify-center'>
+                        <div className='text-black mb-3 font-bold h-16 flex flex-col justify-center text-sm'>
                             {props.values.map((value) => {
                                 return (<p>{value}</p>);
                             })}
--- nextjs_app/app/components/homepage/HeroSection.jsx
+++ nextjs_app/app/components/homepage/HeroSection.jsx
@@ -1,9 +1,29 @@
+"use client"
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+ 
 const HeroSection = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: false })
+  )
+ 
   return (
-    <section className='p-4 bg-cover bg-center' style={{ backgroundImage: `url(/images/hero-background.png)` }}>
-      <div className='md:mt-0 mt-6 pt-[20vw] pb-[24vw] text-center mx-auto h-full'>
-        <h2 className='text-5xl text-light-green font-bold'>Jalan Journey</h2>
-        <p className='text-base sm:text-xl md:text-2xl mt-2 text-white font-bold leading-snug'>Cultivate Deeper Empathy through Play</p>
+    <section className='p-4 relative'>
+      <Carousel plugins={[plugin.current]} className="absolute top-0 bottom-0 left-0 right-0">
+        <CarouselContent className="absolute top-0 bottom-0 left-0 right-0 ml-0">
+          <CarouselItem className="w-full bg-cover bg-center" style={{ backgroundImage: `url(/images/hero-background.jpg)` }}>
+          </CarouselItem>
+          <CarouselItem className="w-full bg-cover bg-center" style={{ backgroundImage: `url(/images/hero-background.jpg)` }}>
+          </CarouselItem>
+          <CarouselItem className="w-full bg-cover bg-center" style={{ backgroundImage: `url(/images/hero-background.jpg)` }}>
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+      <div className="absolute top-0 bottom-0 left-0 right-0 bg-[#644C8F]/20 z-10"></div>
+      <div className='md:mt-0 mt-6 pt-[20vw] pb-[24vw] text-center mx-auto h-full relative z-20'>
+        <h2 className='text-6xl text-[#A2C17C] font-bold' style={{ textShadow: `3px 3px #000000`, "-webkit-text-stroke-width": `1px`, "-webkit-text-stroke-color": `black` }}>Jalan Journey</h2>
+        <p className='text-base sm:text-xl md:text-2xl mt-4 text-white font-bold leading-snug'>Cultivate Deeper Empathy through Play</p>
         <p className='text-sm sm:text-base md:text-lg mt-1 text-white leading-snug'>We create games with meaningful social themes</p>
       </div>
     </section>
